7778adcf117226fd82ee806a0972d0cd3674887c
JamieSWright <jamie.wright@xdesign.com>
2025-12-16 14:10:08 +0000

    checkout page POM outline

---
+++ tests/pages/checkoutpage.ts
@@ -0,0 +1,113 @@
+import { Page, expect } from '@playwright/test';
+
+
+export class CheckoutPage {
+  readonly page: Page;
+  readonly baseUrl = 'https://www.saucedemo.com';
+
+  // Selectors - Step One (Information)
+
+
+  // Selectors - Step Two (Overview)
+
+
+  // Selectors - Complete
+
+
+
+
+
+
+  /**
+   * Navigate to checkout step one
+   */
+
+
+  /**
+   * Navigate to checkout step two
+   */
+
+
+  /**
+   * Navigate to checkout complete page
+   */
+
+
+  /**
+   * Fill in the first name field
+   */
+
+
+  /**
+   * Fill in the last name field
+   */
+
+
+  /**
+   * Fill in the postal code field
+   */
+
+
+  /**
+   * Fill in all shipping information fields
+   */
+
+
+   /**
+   * Click the continue button
+   */
+
+
+    /**
+   * Click the cancel button
+   */
+
+
+    /**
+   * Click the finish button
+   */
+
+
+    /**
+   * Click the back home button
+   */
+
+
+     /**
+   * Get the subtotal amount
+   */
+
+
+      /**
+   * Get the tax amount
+   */
+
+
+      /**
+   * Get the total amount
+   */
+
+
+      /**
+   * Get the completion header text
+   */
+
+
+      /**
+   * Expect completion header to contain specific text
+   */
+
+
+       /**
+   * Expect to be on checkout step one page
+   */
+
+
+       /**
+   * Expect to be on checkout step two page
+   */
+
+
+     /**
+   * Expect to be on checkout complete page
+   */   
